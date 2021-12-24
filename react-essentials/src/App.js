39a9288202be86924a919f0cdcd473075ab442cc
Ulises Mariano Melgarejo <ulimarian2000@gmail.com>
2021-12-24 20:20:03 -0300

    new newww

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,4 +1,8 @@
+import React from 'react';
+
 import './App.css';
+
+import restaurant from "./restaurant.jpg";
 
 function Header(props){
   console.log(props);
@@ -9,6 +13,16 @@ function Header(props){
 function Main(props){
   return<section>
     <p>We serve the most {props.adjective} food around.</p>
+    <img 
+    src={restaurant} 
+    height= {200}
+     alt="restaurant" 
+      />
+    <ul style = {{textAlign: "left"}} >
+      {props.dishes.map((dish)=> 
+      <li key = {dish.id} > {dish.title} </li>)}
+    </ul>
+    
   </section>
 }
 function Footer(props){
@@ -19,12 +33,22 @@ function Footer(props){
   );
 }
 
+const dishes = [
+  "Mac and Cheese",
+  "Pizza",
+  "Hamburger"
+];
+
+
+const dishObjects = dishes.map((dish, i) => ( {id:i, title:dish} ));
+
+dishes.map((dish) => console.log(dish));
 
 function App() {
   return (
     <div className="App">
       <Header name ="Ulises"/>
-      <Main adjective ="amazing"/>
+      <Main adjective ="amazing" dishes = {dishObjects} />
       <Footer year= {new Date().getFullYear()} />
     </div>
   );
--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from "./App";
 
 
+function AppTwo(){
+  return <h1>The second app</h1>;
+  
+}
+
 ReactDOM.render(
 
   < App />,
+
   document.getElementById('root')
 );
 
